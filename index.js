a1390e053c206f68f592305d7aec73a1d1171664
Gabriel Mendes <gabriel.26056@gmail.com>
2022-03-02 15:24:12 -0300

    added click functions

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const decrease = document.querySelector('.decrease')
 const reset = document.querySelector('.reset')
 const increase = document.querySelector('.increase')
 const counterValue = document.querySelector('span')
+
+decrease.onclick = () => {
+  counterValue.textContent -= 1
+
+  changeColor(counterValue)
+}
+
+reset.onclick = () => {
+  counterValue.textContent = 0
+
+  changeColor(counterValue)
+}
+
+increase.onclick = () => {
+  counterValue.textContent++
+
+  changeColor(counterValue)
+}
