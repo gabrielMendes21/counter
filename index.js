53544cfa0a0144ed624c994a433051f4d487f4a9
Gabriel Mendes <gabriel.26056@gmail.com>
2022-03-02 15:23:22 -0300

    added main variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const decrease = document.querySelector('.decrease')
+const reset = document.querySelector('.reset')
+const increase = document.querySelector('.increase')
+const counterValue = document.querySelector('span')
