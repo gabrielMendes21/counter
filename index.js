0595321541e37daa3f00f042eaf61efd9bab42c6
Gabriel Mendes <gabriel.26056@gmail.com>
2022-03-02 15:24:59 -0300

    added 'changeColor' function

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const reset = document.querySelector('.reset')
 const increase = document.querySelector('.increase')
 const counterValue = document.querySelector('span')
 
+function changeColor(element) {
+  if (Math.sign(element.textContent) == -1) {
+    element.style.color = "red"
+  }
+  else if (Math.sign(element.textContent) == 1) {
+    element.style.color = "green"
+  }
+  else if (Math.sign(element.textContent) == 0) {
+    element.style.color = "black"
+  }
+}
+
 decrease.onclick = () => {
   counterValue.textContent -= 1
 
